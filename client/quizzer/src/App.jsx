f96f4395381604b1617348da2ea88152dc9a7755
julesk1702 <89418218+julesk1702@users.noreply.github.com>
2023-10-25 10:29:36 +0200

    leaderboard commit

--- client/quizzer/src/App.jsx
+++ client/quizzer/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Quiz from './components/quiz/Quiz'
 
 function App() {
 
 
   return (
     <div className="App">
-      <Quiz />
     </div>
     )
 }
