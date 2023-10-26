c10a1a602061a3c787ac3c3cf379792eaa5e3f0e
Richard Visser <HJ.Visser@student.han.nl>
2023-10-26 11:10:38 +0200

    done

--- client/quizzer/src/reducers/quizReducer.jsx
+++ client/quizzer/src/reducers/quizReducer.jsx
@@ -1,27 +1,23 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchGamePin = createAsyncThunk(
-  "quiz/fetchGamePin",
-  async () => {
-    const response = await fetch("http://localhost:3000/quiz");
-    const data = await response.json();
-    return data;
-  }
-);
+export const fetchGamePin = createAsyncThunk("quiz/fetchGamePin", async () => {
+  const response = await fetch("http://localhost:3000/quiz");
+  const data = await response.json();
+  return data;
+});
 
 const QuizSlice = createSlice({
   name: "quiz",
   initialState: {
-    gamePin: "",  
+    gamePin: "",
     id: "",
     rounds: [],
     questions: [],
   },
-  reducers: {
-  },
+  reducers: {},
   extraReducers: {
     [fetchGamePin.fulfilled]: (state, action) => {
-      state.gamePin = action.payload;
+      state.gamepin = action.payload;
     },
   },
 });
