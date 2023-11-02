9192634bcdcdc56eb8344bfbe0bba7adf5234c8f
Richard Visser <HJ.Visser@student.han.nl>
2023-11-02 10:51:46 +0100

    push

--- client/quizzer/src/main.jsx
+++ client/quizzer/src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import store from "./store";
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'; // Import PersistGate
+import store, { persistor } from "./store"; // Import your store and persistor
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+    </PersistGate>
   </Provider>
 );
--- client/quizzer/src/store.jsx
+++ client/quizzer/src/store.jsx
@@ -1,16 +1,43 @@
+// store.js
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import quizReducer from "./reducers/quizReducer";
 import teamReducer from "./reducers/teamReducer";
 import answerReducer from "./reducers/answerReducer";
 import quizmasterReducer from "./reducers/quizmasterReducer";
 import roundReducer from "./reducers/roundReducer";
 
-export default configureStore({
-  reducer: {
-    quiz: quizReducer,
-    team: teamReducer,
-    answer: answerReducer,
-    quizmaster: quizmasterReducer,
-    round: roundReducer,
-  },
+// Import redux-persist and add it to your store configuration
+import { combineReducers } from 'redux';
+// import { createWebStorage } from 'redux-persist';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+// Combine all your reducers into a single root reducer
+const rootReducer = combineReducers({
+  quiz: quizReducer,
+  team: teamReducer,
+  answer: answerReducer,
+  quizmaster: quizmasterReducer,
+  round: roundReducer,
 });
+
+// Configure Redux Persist
+const persistConfig = {
+  key: 'root', // key is a unique identifier for your store
+  storage, // the storage engine to use (localStorage by default)
+  // You can customize the storage configuration further if needed
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create your Redux store
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Create a Redux Persistor
+export const persistor = persistStore(store);
+
+// Export both the store and the persistor
+export default store;
